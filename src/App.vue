df0eead870467f6996313003b336a623405aec44
«Vasiliy <v_bibas@mail.ru>
2024-02-04 13:33:39 +0300

    added files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Main/>
+  <Blog/>
+  <BlogDetails/>
+  <ProductDetails/>
+
+</template>
+
+<script>
+import Main from './components/Main.vue';
+import Blog from './components/Blog.vue';
+import BlogDetails from './components/BlogDetails.vue';
+import ProductDetails from './components/ProductDetails.vue';
+
+
+
+export default {
+  name: 'App',
+  components: {
+    Main,
+    Blog,
+    BlogDetails,
+    ProductDetails
+}
+}
+</script>
+
+<style lang="scss">
+
+</style>
